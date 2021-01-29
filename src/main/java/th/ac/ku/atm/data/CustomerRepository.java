37f7463833e862d1ff18d6e272b129112b3ec57c
M-thut <maethut.p@ku.th>
2021-01-30 05:20:35 +0700

    add CustomerRestController >> lab 8.1

--- src/main/java/th/ac/ku/atm/data/CustomerRepository.java
+++ src/main/java/th/ac/ku/atm/data/CustomerRepository.java
@@ -2,7 +2,7 @@ package th.ac.ku.atm.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import th.ac.ku.model.Customer;
+import th.ac.ku.atm.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
